8acaedf03aa59f777f827d17dc58fd4188c0eec3
dudezi <a01036952858@gmail.com>
2024-09-23 09:14:11 +0900

    first commit

---
+++ src/main/java/com/example/sbb_test2_1/SbbTest21Application.java
@@ -0,0 +1,13 @@
+package com.example.sbb_test2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbTest21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbTest21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbb_test2_1/SbbTest21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb_test2_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbTest21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
