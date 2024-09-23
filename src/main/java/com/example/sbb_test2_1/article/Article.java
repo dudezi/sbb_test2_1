d9a6e6bcb7ddca40bc520523ee057e176d49fa91
dudezi <a01036952858@gmail.com>
2024-09-23 09:26:37 +0900

    feat:Article 추가

---
+++ src/main/java/com/example/sbb_test2_1/article/Article.java
@@ -0,0 +1,26 @@
+package com.example.sbb_test2_1.article;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Article {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime creteDate;
+}
---
+++ src/main/java/com/example/sbb_test2_1/article/ArticleService.java
@@ -0,0 +1,4 @@
+package com.example.sbb_test2_1.article;
+
+public class ArticleService {
+}
